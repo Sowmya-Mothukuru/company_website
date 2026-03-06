e11b3806e77ccc9fd319b4b6730005e0e652d00d
Sowmya <rr2000072somi@gmail.com>
2026-03-06 18:38:29 +0530

    homepagechangesbysowmya

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${spaceGrotesk.className} bg-white`}>
-        <Navbar/>
+        <Navbar />
         {children}
       </body>
     </html>
